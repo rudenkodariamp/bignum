1243186ad19a565bd79b73332bc36248cde80a6c
Даша <dasha@DashaPC.(none)>
2013-06-27 11:13:54 +0400

    Модуль с++

---
+++ bignumcpp.h
@@ -0,0 +1,30 @@
+extern "C" {
+#include "bignumc.h"
+};
+
+class Long {
+private:
+	bignum num;
+public:
+	Long(const char *);
+	~Long();
+	Long(const Long &);
+	Long &operator =(const Long&); 
+	const Long operator +(Long&);
+	const Long operator -(Long&);
+	Long operator -();
+	const Long operator *(Long&);
+	const Long operator /(Long&);
+	const Long operator %(Long&);
+
+	bool operator ==(const Long&);
+	bool operator <(const Long&);
+	bool operator >(const Long&);
+	bool operator <=(const Long&);
+	bool operator >=(const Long&);
+
+	Long(const bignum&);
+	const bignum& getnum() const;
+	void outf(const char *);
+	void out();
+};
